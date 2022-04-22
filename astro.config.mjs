a09d0afcd4cbd1290350fbd54fc9d49f389b5ff9
panesarpbx8 <panesar.pb08@gmail.com>
2022-04-22 11:41:33 -0400

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import turbolinks from "@astrojs/turbolinks";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact(), turbolinks()],
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   }
 });
