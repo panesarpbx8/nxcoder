3404ef9427f50ee4fe6a1d88da6386890b522307
panesarpbx8 <panesar.pb08@gmail.com>
2022-04-25 12:32:50 -0400

    polishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nxcoder.vercel.app',
   integrations: [turbolinks()],
   markdown: {
     syntaxHighlight: 'prism',
   },
+
 });
